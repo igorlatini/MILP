e150f00ecdd2571b645cdbcd54be90ac2fbb6798
igorlatini <igorlatini@gmail.com>
2025-07-13 20:48:20 -0300

    Added manhattan distance
    Correction p1 and p2 from float to Sequence[float]

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 import math
+from typing import Literal, Sequence
 
-def travel_time_position(p1: float, p2: float, v: float):
+
+def travel_time_position(p1: Sequence[float], p2: Sequence[float], v: float, type: Literal['EUCLIDEAN', 'MANHATTAN'] = 'EUCLIDEAN'):
     if v <= 0:
         raise ValueError("Invalid speed")
-    return round(math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)/v, 2)
+    if type == 'EUCLIDEAN':
+        return round(math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)/v, 2)
+    elif type == 'MANHATTAN':
+        return round((abs(p2[0] - p1[0]) + abs(p2[1] - p1[1])) /v, 2)
 
 def travel_time_distance(d: float, v: float):
     if v <= 0:
